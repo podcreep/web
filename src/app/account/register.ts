dcb5634c88f5b61e72ae067373398bad2a806993
Dean Harding <dean@codeka.com.au>
2018-04-29 13:13:48 -0700

    Implement the call to save accounts.

--- src/app/account/register.ts
+++ src/app/account/register.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { AbstractControl, FormControl, FormGroupDirective, NgForm, Validators, ValidationErrors } from '@angular/forms';
+import { AsyncValidatorFn, FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { AccountService, UsernameStatus } from '../../services/account';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class UsernameErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
 
 @Component({
   selector: 'register',
@@ -20,28 +13,47 @@ export class UsernameErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./register.css']
 })
 export class RegisterComponent {
-  usernameFormControl = new FormControl(
-    '',
-    [Validators.required],
-    [this.isUserNameTaken.bind(this)]);
+  registerForm: FormGroup;
 
-  usernameErrorStateMatcher = new UsernameErrorStateMatcher();
+  constructor(
+    private readonly accountService: AccountService,
+    private fb: FormBuilder,
+    private readonly router: Router) {
 
-  constructor(private readonly accountService: AccountService) {}
+    this.registerForm = this.fb.group({
+      username: new FormControl('', [Validators.required], [this.userNameValidator()]),
+      password: new FormControl('', [Validators.required]),
+      password2: new FormControl('', [Validators.required]),
+    });
+  }
 
-  isUserNameTaken(ctrl: AbstractControl): Observable<ValidationErrors> {
-    return this.accountService.getUsernameStatus(ctrl.value)
-        .map(status => {
-          switch(status) {
-            case UsernameStatus.AVAILABE:
-              return null;
-            case UsernameStatus.UNAVAILABLE:
-              return { username: "unavailable" };
-            case UsernameStatus.INVALID:
-              return { username: "invalid" };
-            default:
-              return { username: "unknown" };
-          }
-        });
+  userNameValidator(): AsyncValidatorFn {
+    return (ctrl: AbstractControl): Observable<ValidationErrors | null> => {
+      return this.accountService.getUsernameStatus(ctrl.value)
+          .map(status => {
+            switch(status) {
+              case UsernameStatus.AVAILABE:
+                return null;
+              case UsernameStatus.UNAVAILABLE:
+                return { username: "unavailable" };
+              case UsernameStatus.INVALID:
+                return { username: "invalid" };
+              default:
+                return { username: "unknown" };
+            }
+          });
+    }
+  }
+
+  register() {
+    if (!this.registerForm.valid) {
+      return;
+    }
+    this.accountService.register(
+      this.registerForm.controls['username'].value,
+      this.registerForm.controls['password'].value).subscribe(
+        data => { this.router.navigate(['podcasts']); },
+        err => { /* todo: show the error */ console.log(err); }
+      );
   }
 }
--- src/services/account.ts
+++ src/services/account.ts
@@ -8,11 +8,22 @@ import 'rxjs/add/operator/delay';
 
 import { ENV } from '../environments/environment';
 
+/**
+ * The status we'll return for a username.
+ */
 export enum UsernameStatus {
+  /** The username doesn't exist on the server, and is available to be used. */
   AVAILABE,
+  /** The username is already registered. */
   UNAVAILABLE,
+  /** The username is somehow invalid. */
   INVALID,
+  /** Some other error occured. */
   UNKNOWN,
+}
+
+interface RegisterResponse {
+  cookie: string;
 }
 
 @Injectable()
@@ -31,7 +42,6 @@ export class AccountService {
         params: { username: username },
       })
       .map(resp => {
-        console.log("here we are");
         // Any 200 response means the username was in the database already.
         return UsernameStatus.UNAVAILABLE;
       })
@@ -42,5 +52,26 @@ export class AccountService {
           return of(UsernameStatus.INVALID);
         }
       }));
+  }
+
+  
+
+  /**
+   * Register the user on the server, returns the cookie you should use to authenticate further
+   * requests.
+   *
+   * @param username The username to register.
+   * @param password The password for the user.
+   * @returns An {@link Observable} of the cookie to be saved and used in subsequent requests.
+   */
+  register(username: string, password: string): Observable<string> {
+    return this.httpClient
+      .post<RegisterResponse>(ENV.BACKEND + 'api/accounts', {
+        username: username,
+        password: password
+      })
+      .map(resp => {
+        return resp.cookie;
+      });
   }
 }
