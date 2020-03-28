116bacdc1fb255ae92d530b22ef109c8d293965f
Dean Harding <dean@codeka.com.au>
2020-03-28 12:21:49 -0700

    Include the list of in-progress episodes as well.

--- src/app/podcasts/subscriptions.ts
+++ src/app/podcasts/subscriptions.ts
@@ -5,6 +5,7 @@ import { EpisodeDetailsComponent } from './episode_details';
 
 import { Episode, Podcast, PodcastsService, Subscription } from '../../services/podcasts';
 import { PlaybackService } from '../../services/playback';
+import { formatTime } from '../../services/utils';
 
 class EpisodeDetails {
   episode: Episode
@@ -19,6 +20,9 @@ class EpisodeDetails {
 export class SubscriptionsComponent {
   subscriptions: Subscription[];
   newEpisodes: EpisodeDetails[];
+  inProgress: EpisodeDetails[];
+
+  formatTime = formatTime;
 
   constructor(
     private readonly podcastsService: PodcastsService,
@@ -31,12 +35,21 @@ export class SubscriptionsComponent {
       this.subscriptions = subscriptionList.subscriptions;
       const podcasts = new Map<Number, Podcast>();
       for (const s of this.subscriptions) {
+        s.podcast.subscription = s;
         podcasts[s.podcastID] = s.podcast;
       }
       this.newEpisodes = [];
       for (const ep of subscriptionList.newEpisodes) {
-        const podcast = podcasts[ep.podcastID]
+        const podcast = podcasts[ep.podcastID];
         this.newEpisodes.push({
+          episode: ep,
+          podcast: podcast,
+        });
+      }
+      this.inProgress = [];
+      for (const ep of subscriptionList.inProgress) {
+        const podcast = podcasts[ep.podcastID];
+        this.inProgress.push({
           episode: ep,
           podcast: podcast,
         });
